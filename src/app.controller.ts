071c871d7bd3e6e1d7448b24000bb9e949a2f34a
Prasenjit <prasenjit.aluni@gmail.com>
2025-04-23 10:25:09 +0530

    API gateway

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { timeout, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject('USER_SERVICE') private readonly userClient: ClientProxy) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get('users')
+  // getUsers() {
+  //   return this.userClient.send({ cmd: 'get_users' }, {});
+  // }
+
+
+  @Get('users')
+  getUsers() {
+    return this.userClient
+      .send({ cmd: 'get_users' }, {})
+      .pipe(
+        timeout(5000),
+        catchError(() => of({ message: 'User service not available' }))
+      );
   }
 }
+
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.TCP,
+        options: { host: 'localhost', port: 3001 },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
