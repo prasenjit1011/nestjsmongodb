68e69c34d62a15a28a6dcaa20d239c486d17ebe0
Prasenjit <prasenjit.aluni@gmail.com>
2025-04-27 11:30:20 +0530

    Port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config(); // Load .env before anything else
+  console.log('PORT : ', process.env.PORT)
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
