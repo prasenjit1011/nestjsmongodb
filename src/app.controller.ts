b6d0f2aa1f8231e61581e953b48792c16378f570
Prasenjit <prasenjit.aluni@gmail.com>
2025-04-28 13:40:27 +0530

    Microservice with Nestjs Api Gateway and TCP

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @MessagePattern({ cmd: 'get_users' })
+  users(data: any) {
+    const msg = 'User Details '+(new Date).getMilliseconds();
+    return { success: true, msg , user: data.username };
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    const msg = 'Hello World! '+(new Date).getMilliseconds();
+    return {msg};
   }
 }
