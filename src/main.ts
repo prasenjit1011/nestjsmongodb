6a7bb61c6f14637c24967e5ef55263271899b318
Prasenjit <prasenjit.aluni@gmail.com>
2025-04-28 13:40:07 +0530

    Microservice with Nestjs Api Gateway and TCP

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   dotenv.config(); // Load .env before anything else
   console.log('PORT : ', process.env.PORT)
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 4002,
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(4012);
 }
 bootstrap();
