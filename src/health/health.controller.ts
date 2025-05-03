6fd2f414fd2cf78d60678edbf8a141ae706c0cd6
Prasenjit <prasenjit.aluni@gmail.com>
2025-05-03 11:12:19 +0530

    terminus

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,19 +1,22 @@
 
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheckService, HttpHealthIndicator, HealthCheck } from '@nestjs/terminus';
+import { HealthCheckService, HttpHealthIndicator, HealthCheck, MemoryHealthIndicator } from '@nestjs/terminus';
+
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
+    private memory: MemoryHealthIndicator,
   ) {}
 
   @Get()
   @HealthCheck()
   check() {
     return this.health.check([
-      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+      () => this.memory.checkHeap('memory_heap', 300 * 1024 * 1024), // 300 MB heap
+      () => this.memory.checkRSS('memory_rss', 500 * 1024 * 1024),   // 500 MB RSS
     ]);
   }
 }
