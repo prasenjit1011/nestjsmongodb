1f4942b7965589d059df554cffe391512c935f8c
Prasenjit <prasenjit.aluni@gmail.com>
2025-05-18 09:17:02 +0530

    c 916

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Nestjs Lambda API Server Setup.. SNS 857-910 : '+(new Date).getMilliseconds();
+    return 'Nestjs Lambda API Server Setup.. SNS 916 : '+(new Date).getMilliseconds();
   }
 }
