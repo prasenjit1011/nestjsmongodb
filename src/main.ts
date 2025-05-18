fa3dfe1a82c026673b4891384ebff2f2ef9a7957
Prasenjit <prasenjit.aluni@gmail.com>
2025-05-18 13:21:21 +0530

    commit 1321

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { graphqlUploadExpress } from 'graphql-upload-ts';
-import { RedisIoAdapter } from './websocket/redisioadapter';
+//import { RedisIoAdapter } from './websocket/redisioadapter';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import * as dotenv from 'dotenv';
@@ -12,9 +12,9 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   
-  const redisIoAdapter = new RedisIoAdapter(app);
-  await redisIoAdapter.connectToRedis();
-  app.useWebSocketAdapter(redisIoAdapter);
+  // const redisIoAdapter = new RedisIoAdapter(app);
+  // await redisIoAdapter.connectToRedis();
+  // app.useWebSocketAdapter(redisIoAdapter);
 
   app.use('/uploads', express.static('uploads'));
   app.use(graphqlUploadExpress({ maxFileSize: 10_000_000, maxFiles: 1 }));
