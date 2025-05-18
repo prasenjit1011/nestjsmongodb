1fee696a27b4cdee44957ba4babad238c0c759e7
Prasenjit <prasenjit.aluni@gmail.com>
2025-05-18 06:48:31 +0530

    commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Nestjs Lambda API Server Setup.... Action 01-17052025--2247 : '+(new Date).getMilliseconds();
+    return 'Nestjs Lambda API Server Setup....hello Action 01-17052025--2247 : '+(new Date).getMilliseconds();
   }
 }
