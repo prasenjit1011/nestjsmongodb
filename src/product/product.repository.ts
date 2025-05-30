08ee3a2c99777b1d6d517aecbecdaa86970d2bf1
Prasenjit <prasenjit.aluni@gmail.com>
2025-05-30 19:11:30 +0530

    commit 1911

--- src/product/product.repository.ts
+++ src/product/product.repository.ts
@@ -32,7 +32,14 @@ export class ProductRepository {
     });
   
     const response = await sqs.send(command);
-  
+ 
+    const dtd = new Date;
+    const str = 'Dtd : '+dtd.getDate()+' -:- '+dtd.getHours()+':'+dtd.getMinutes()+':'+dtd.getSeconds()+':'+dtd.getMilliseconds();
+    const prodData = {name:"Test - "+str, price:123, description:"Dummy"};
+    const createdProduct = new this.productModel(prodData);
+
+
+
     if (response.Messages && response.Messages.length > 0) {
       const message = response.Messages[0];
       const body = JSON.parse(message.Body);
@@ -42,11 +49,7 @@ export class ProductRepository {
         QueueUrl: queueUrl,
         ReceiptHandle: message.ReceiptHandle,
       }));
-  
-      const dtd = new Date;
-      const str = 'Dtd : '+dtd.getDate()+' -:- '+dtd.getHours()+':'+dtd.getMinutes()+':'+dtd.getSeconds()+':'+dtd.getMilliseconds();
-      const prodData = {name:"Test - "+str, price:123, description:"Dummy"};
-      const createdProduct = new this.productModel();
+ 
       return body;
     } else {
       return { message: "No messages in the myqueue... 123 : "+(new Date).getMilliseconds(), response};
