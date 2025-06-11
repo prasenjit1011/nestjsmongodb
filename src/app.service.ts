a17a09c98491bfaed62a266ed85b6fbd26337f06
Prasenjit <prasenjit.aluni@gmail.com>
2025-06-11 10:43:31 +0530

    Commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Nestjs lambda_01_cicd_04_sqs_01 : 07June2025 : SQS : 623 '+(new Date).getMilliseconds();
+    return 'Nestjs lambda_01_cicd_04_sqs_01 : 11June2025 : SQS : 1043 '+(new Date).getMilliseconds();
   }
 }
