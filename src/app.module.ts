e8f645082582421d1130854effe48274dcdc5ce6
Prasenjit <prasenjit.aluni@gmail.com>
2025-06-11 12:32:54 +0530

    Commit 1232

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { AppService } from './app.service';
 //import { ProductModule } from './product/product.module';
 //import { ProductController } from './product/product.controller';
 import { ProductModule } from './product/product.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    }),
     MongooseModule.forRoot('mongodb+srv://tester:tester1234@cluster0.hlicuim.mongodb.net/demodb?retryWrites=true&w=majority'),
     ProductModule,
     //ProductModule
